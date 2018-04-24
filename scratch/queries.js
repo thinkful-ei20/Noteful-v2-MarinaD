8632b87e908c4a61639a8c8825541864e011da7c
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-24 12:27:27 -0600

    Set up scratch knex queries for integration

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,19 +2,59 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
-knex
-  .select('notes.id', 'title', 'content')
-  .from('notes')
-  .modify(queryBuilder => {
-    if (searchTerm) {
-      queryBuilder.where('title', 'like', `%${searchTerm}%`);
-    }
-  })
-  .orderBy('notes.id')
-  .then(results => {
-    console.log(JSON.stringify(results, null, 2));
-  })
-  .catch(err => {
-    console.error(err);
-  });
+//Get all notes
+// let searchTerm = 'golden';
+// knex
+//   .select('notes.id', 'title', 'content')
+//   .from('notes')
+//   .modify(queryBuilder => {
+//     if (searchTerm) {
+//       queryBuilder.where('title', 'like', `%${searchTerm}%`);
+//     }
+//   })
+//   .orderBy('notes.id')
+//   .then(results => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// ** Get note by id
+// let id = 1003;
+// knex('notes')
+//   .select('notes.id','title','content')
+//   .where('notes.id', id)
+//   .then(results=> console.log(results[0]))
+//   .catch(err => console.log(err));
+
+//**Update note by ID
+// let id = 1003;
+// let updateObj = {title : 'Brand new new new knexy title'};
+// knex('notes')
+//   .update(updateObj)
+//   .where('notes.id',id)
+//   .returning(['notes.id','title','content'])
+//   .then(results => console.log(results[0]))
+//   .catch(err => console.log(err));
+
+// let newObj = {
+//   title : 'my sweet title',
+//   content: 'my sweet content'
+// };
+// knex('notes')
+//   .insert(newObj)
+//   .returning(['notes.id','title','content'])
+//   .then(results => console.log(results[0]))
+//   .catch(err=> console.log(err));
+
+// let id = 1003;
+// knex('notes')
+//   .del()
+//   .where('notes.id',id)
+//   .then(console.log('success!'))
+//   .catch (err=> console.log(err));
+
+
+
+  
